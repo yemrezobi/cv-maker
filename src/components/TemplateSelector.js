b31fca3361d376e6b97d9c155cbe78a2a1111011
Alhamza <e170503110@stud.tau.edu.tr>
2021-06-14 13:38:11 +0300

    Templates selection is ready

--- src/components/TemplateSelector.js
+++ src/components/TemplateSelector.js
@@ -1,10 +1,68 @@
-function TemplateSelector(){
+import { useDispatch } from "react-redux";
+import Tampalte1 from './Templates/Tamplate1';
+import Tampalte2 from './Templates/Tamplate2';
+import Tampalte3 from './Templates/Tamplate3';
+
+import tm1 from './Icons/tm1.png';
+
+function TemplateSelector(props){
+
+    const dispatch = useDispatch();
+
+    const replaceTemplate = (e) => {
+        dispatch({
+            type: "REMOVE_SECTION",
+            section:  <Tampalte1/> ,
+            panel : "main"
+    
+        });
+    };
+    
+
+    const handleClick1 = (e) => {
+        replaceTemplate();
+        dispatch({
+            type: "ADD_SECTION",
+            section:  <Tampalte1/> ,
+            panel : "main"
+
+        });
+        
+    }
+
+    const handleClick2 = (e) => {
+        replaceTemplate();
+        dispatch({
+            type: "ADD_SECTION",
+            section:<Tampalte2/> ,
+            panel : "main"
+        });
+    }
+
+    const handleClick3 = (e) => {
+        replaceTemplate();
+        dispatch({
+            type: "ADD_SECTION",
+            section:<Tampalte3/> ,
+            panel : "main"
+        });
+    }
 
     return(
-        <div className="select-template box">
-            <p>template selector placeholder</p>
+        <div className="select-section box">
+            <button onClick={handleClick1}>
+                <img className="templateIcon" src = {tm1}></img>
+            </button>
+            <button onClick={handleClick2}>
+                <img className="templateIcon" src = {tm1}></img>     
+           </button>
+           <button onClick={handleClick3}>
+                <img className="templateIcon" src = {tm1}></img>     
+           </button>
+
+
         </div>
-    )
+    );   
 }
 
 export default TemplateSelector
